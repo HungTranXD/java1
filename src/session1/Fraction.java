db3666349efffc4dfdfcdc505cea7cdb16ac48b3
HungTranXD <hungtranxd2697@gmail.com>
2022-10-12 17:14:02 +0700

    BTVN buoi 1

--- src/session1/Fraction.java
+++ src/session1/Fraction.java
@@ -1,5 +1,192 @@
 package session1;
 
 public class Fraction {
+    /*-----------------------------------------------------------*/
+    /* Cac thuoc tinh: tu so (numerator) va mau so (denominator) */
+    /*-----------------------------------------------------------*/
+    private int numerator, denominator; //stores the fraction data
+
+    /*----------------------------------*/
+    /* Cac phuong thuc getter va setter */
+    /*----------------------------------*/
+    public int getNumerator() {
+        return numerator;
+    }
+
+    public void setNumerator(int numerator) {
+        this.numerator = numerator;
+    }
+
+    public int getDenominator() {
+        return denominator;
+    }
+
+    public void setDenominator(int denominator) {
+        if (denominator == 0) {
+            throw new IllegalArgumentException("Invalid (zero) denominator.");
+        } else {
+            this.denominator = denominator;
+        }
+    }
+
+    /*------------------------*/
+    /* Phuong thuc in phan so */
+    /*------------------------*/
+    public String toString() {
+        if (numerator * denominator <= 0) {
+            return "-" + Math.abs(numerator) + "/" + Math.abs(denominator);
+        } else {
+            return Math.abs(numerator) + "/" + Math.abs(denominator);
+        }
+    }
+
+    /*-----------------------------*/
+    /* Phuong thuc rut gon phan so */
+    /*-----------------------------*/
+    public Fraction reduce() {
+        Fraction result = new Fraction();
+        int common = 0;
+        // get absolute values for numerator and denominator
+        int num = Math.abs(numerator);
+        int den = Math.abs(denominator);
+        // figure out which is less, numerator or denominator
+        if (num > den)
+            common = gcd(num, den);
+        else if (num < den)
+            common = gcd(den, num);
+        else  // if both are the same, don't need to call gcd
+            common = num;
+
+        // set result based on common factor derived from gcd
+        result.numerator = numerator / common;
+        result.denominator = denominator / common;
+        return result;
+    }
+
+    /*--------------------------------*/
+    /* Phuong thuc nghich dao phan so */
+    /*--------------------------------*/
+    public Fraction inverse() {
+        Fraction result = new Fraction();
+        if (numerator == 0) {
+            throw new IllegalArgumentException("Fractions with numerator = 0 dont have inverse");
+        } else {
+            result.numerator = denominator;
+            result.denominator = numerator;
+        }
+        return result;
+    }
+
+    /*----------------------------*/
+    /* Phuong thuc cong 2 phan so */
+    /*----------------------------*/
+    public Fraction add(Fraction b)
+    {
+        // find lowest common denominator
+        int common = lcd(denominator, b.denominator);
+        // convert fractions to lcd
+        Fraction commonA = new Fraction();
+        Fraction commonB = new Fraction();
+        commonA = convert(common);
+        commonB = b.convert(common);
+        // create new fraction to return as sum
+        Fraction sum = new Fraction();
+        // calculate sum
+        sum.numerator = commonA.numerator + commonB.numerator;
+        sum.denominator = common;
+        // reduce the resulting fraction
+        sum = sum.reduce();
+        return sum;
+    }
+
+    /*---------------------------*/
+    /* Phuong thuc tru 2 phan so */
+    /*---------------------------*/
+    public Fraction subtract(Fraction b)
+    {
+        // find lowest common denominator
+        int common = lcd(denominator, b.denominator);
+        // convert fractions to lcd
+        Fraction commonA = new Fraction();
+        Fraction commonB = new Fraction();
+        commonA = convert(common);
+        commonB = b.convert(common);
+        // create new fraction to return as difference
+        Fraction diff = new Fraction();
+        // calculate difference
+        diff.numerator = commonA.numerator - commonB.numerator;
+        diff.denominator = common;
+        // reduce the resulting fraction
+        diff = diff.reduce();
+        return diff;
+    }
+
+    /*----------------------------*/
+    /* Phuong thuc nhan 2 phan so */
+    /*----------------------------*/
+    public Fraction multiply(Fraction b)
+    {
+        // create new fraction to return as product
+        Fraction product = new Fraction();
+        // calculate product
+        product.numerator = numerator * b.numerator;
+        product.denominator = denominator * b.denominator;
+        // reduce the resulting fraction
+        product = product.reduce();
+        return product;
+    }
+
+    /*----------------------------*/
+    /* Phuong thuc chia 2 phan so */
+    /*----------------------------*/
+    public Fraction divide(Fraction b)
+    {
+        // create new fraction to return as result
+        Fraction result = new Fraction();
+        // calculate result
+        result.numerator = numerator * b.denominator;
+        result.denominator = denominator * b.numerator;
+        // reduce the resulting fraction
+        result = result.reduce();
+        return result;
+    }
+
+
+
+
+
+
+    /*---------------------------------------------------*/
+    /* private methods used internally by Fraction class */
+    /*---------------------------------------------------*/
+
+    // 1. Find LCD (find lowest common denominator, used to add and subtract fractions)
+    private int lcd(int denom1, int denom2)
+    {
+        int factor = denom1;
+        while ((denom1 % denom2) != 0)
+            denom1 += factor;
+        return denom1;
+    }
+    // 2. Find GCD (find greatest common denominator, used to reduce fractions) - Eculid algorithm
+    private int gcd(int denom1, int denom2)
+    {
+        int factor = denom2;
+        while (denom2 != 0) {
+            factor = denom2;
+            denom2 = denom1 % denom2;
+            denom1 = factor;
+        }
+        return denom1;
+    }
+    // 3. Convert (convert a fraction to an equivalent one based on a lowest common denominator)
+    private Fraction convert(int common)
+    {
+        Fraction result = new Fraction();
+        int factor = common / denominator;
+        result.numerator = numerator * factor;
+        result.denominator = common;
+        return result;
+    }
 
 }
--- src/session1/Main.java
+++ src/session1/Main.java
@@ -4,25 +4,46 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Student s = new Student();
-        s.name = "Nam";
-        s.age = 18;
-
-        Student s2 = new Student();
-        s2.age = 22;
-
-        s.showInfo();
-        s2.showInfo();
+//        Student s = new Student();
+//        s.name = "Nam";
+//        s.age = 18;
+//
+//        Student s2 = new Student();
+//        s2.age = 22;
+//
+//        s.showInfo();
+//        s2.showInfo();
+//
+//        Scanner sc = new Scanner(System.in);
+//        Triangle t1 = new Triangle();
+//        System.out.println("Nhap canh 1:");
+//        t1.setA(sc.nextInt());
+//        System.out.println("Nhap canh 2:");
+//        t1.setB(sc.nextInt());
+//        System.out.println("Nhap canh 3:");
+//        t1.setC(sc.nextInt());
+//        System.out.println("Parameter: " + t1.findParameter());
+//        System.out.println("Area: " + t1.findArea());
 
         Scanner sc = new Scanner(System.in);
-        Triangle t1 = new Triangle();
-        System.out.println("Nhap canh 1:");
-        t1.setA(sc.nextInt());
-        System.out.println("Nhap canh 2:");
-        t1.setB(sc.nextInt());
-        System.out.println("Nhap canh 3:");
-        t1.setC(sc.nextInt());
-        System.out.println("Parameter: " + t1.findParameter());
-        System.out.println("Area: " + t1.findArea());
+        Fraction f1 = new Fraction();
+        System.out.println("Nhap tu so:");
+        f1.setNumerator(sc.nextInt());
+        System.out.println("Nhap mau so:");
+        f1.setDenominator(sc.nextInt());
+        System.out.println("Phan so vua nhap: " + f1.toString());
+        System.out.println("Rut gon:" + f1.reduce());
+        System.out.println("Nghich dao: " + f1.inverse());
+
+        Fraction f2 = new Fraction();
+        System.out.println("Nhap tu so:");
+        f2.setNumerator(sc.nextInt());
+        System.out.println("Nhap mau so:");
+        f2.setDenominator(sc.nextInt());
+
+        System.out.println("Ket qua phep cong: " + f1.add(f2));
+        System.out.println("Ket qua phep tru: " + f1.subtract(f2));
+        System.out.println("Ket qua phep nhan: " + f1.multiply(f2));
+        System.out.println("Ket qua phep chia: " + f1.divide(f2));
     }
 }
