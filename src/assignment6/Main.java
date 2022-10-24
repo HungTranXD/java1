dd57c6f1b85b6b8e8ae848fb8455e8994f18d586
HungTranXD <hungtranxd2697@gmail.com>
2022-10-24 08:18:36 +0700

    assignment6

--- src/assignment6/Main.java
+++ src/assignment6/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("student.fxml"));
-        primaryStage.setTitle("Assignment 5");
+        primaryStage.setTitle("Assignment 6");
         primaryStage.setScene(new Scene(root,800,600));
         primaryStage.show();
     }
